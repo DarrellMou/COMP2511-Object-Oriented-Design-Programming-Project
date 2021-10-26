25952b6d857012aaca33cbefdf2e85c7c987249e
Daniel Ngo <z5257478@unsw.edu.au>
2021-10-26 20:19:40 +1100

    Rename Arrows class to Arrow

---
+++ src/main/java/Entities/collectableEntities/materials/Arrow.java
@@ -0,0 +1,20 @@
+package Entities.collectableEntities.materials;
+
+import Entities.InventoryItem;
+import Entities.collectableEntities.ConsumableEntity;
+import dungeonmania.util.Position;
+
+public class Arrow extends ConsumableEntity {
+
+    public Arrow(String id, String type, Position position, boolean isInteractable) {
+        super(id, type, position, isInteractable);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void consumeItem() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
