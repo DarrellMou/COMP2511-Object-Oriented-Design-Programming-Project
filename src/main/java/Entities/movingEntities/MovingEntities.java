bff892fbcb7e8a9ab505c934d089e9cd718cf35e
Sharon Zhou <sharonzhou@Sharons-MacBook-Air.local>
2021-10-27 23:58:05 +1100

    Fixed spider movement

--- src/main/java/Entities/movingEntities/MovingEntities.java
+++ src/main/java/Entities/movingEntities/MovingEntities.java
@@ -7,7 +7,7 @@ import dungeonmania.util.Position;
 
 import java.util.List;
 
-public  class MovingEntities extends Entities implements Movable {
+public abstract class MovingEntities extends Entities implements Movable {
     private double health;
     private double attackDamage;
 
@@ -71,12 +71,5 @@ public  class MovingEntities extends Entities implements Movable {
             return true;
         }
         return false;
-    }
-
-
-    @Override
-    public void makeMovement(Position startingPosition, Entities entity, DungeonManiaController controller) {
-        // TODO Auto-generated method stub
-        
     }
 }
--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -75,22 +75,6 @@ public class DungeonManiaController {
 
     
     /** 
-     * @return EntitiesFactory
-     */
-    public EntitiesFactory getEntitiesFactory() {
-        return this.entitiesFactory;
-    }
-
-    
-    /** 
-     * @param entitiesFactory
-     */
-    public void setEntitiesFactory(EntitiesFactory entitiesFactory) {
-        this.entitiesFactory = entitiesFactory;
-    }
-
-    
-    /** 
      * @return Random
      */
     public Random getRandom() {
@@ -519,11 +503,12 @@ public class DungeonManiaController {
 
         spawnEnemies(); // Spawn Enemies
         for (Entities entity: getEntities()) {
-                if (entity instanceof Spider) {
-                    Spider spider = (Spider) entity;
-                    spider.makeMovement(new Position(0, 9), entity, this);
+            if (entity instanceof Spider) {
 
-                }
+                Spider spider = (Spider) entity;
+                spider.makeMovement(spider.getSpawnPosition(), this);
+
+            }
 
 
             
@@ -638,12 +623,12 @@ public class DungeonManiaController {
 
     public void spawnEnemies() {
         if (dungeon.getTicksCounter() % 10 == 0) {
-            Entities spider = entitiesFactory.createEntities("spider", new Position(random.nextInt(10), random.nextInt(10), 2));
+            Entities spider = EntitiesFactory.createEntities("spider", new Position(random.nextInt(10), random.nextInt(10), 2));
             dungeon.addEntities(spider);
         }
 
         if (dungeon.getTicksCounter() % 20 == 0) {
-            Entities zombieToast = entitiesFactory.createEntities("zombie_toast", new Position(random.nextInt(10), random.nextInt(10)));
+            Entities zombieToast = EntitiesFactory.createEntities("zombie_toast", new Position(random.nextInt(10), random.nextInt(10)));
             dungeon.addEntities(zombieToast);
         }
     }
