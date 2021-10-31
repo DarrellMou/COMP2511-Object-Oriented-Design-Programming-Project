80eb1f6b6432fc0046ac7d3fec4c693dd0948bc4
Daniel Ngo <z5257478@unsw.edu.au>
2021-10-31 21:53:50 +1100

    boulder win

---
+++ src/test/java/dungeonmania/GoalsTest.java
@@ -0,0 +1,23 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.util.Direction;
+
+public class GoalsTest {
+    @Test
+    public void testBouldersWin() {
+        DungeonManiaController controller = new DungeonManiaController();
+        controller.newGame("boulders-goals-test", "Standard");
+
+        controller.tick("", Direction.RIGHT);
+        controller.tick("", Direction.RIGHT);
+        controller.tick("", Direction.LEFT);
+        controller.tick("", Direction.DOWN);
+        controller.tick("", Direction.RIGHT);
+
+        assertEquals("", controller.getDungeon().getGoals());
+    }
+}
