9930c379ad5ef9cdc41bae0fb16a9d8bcc1e7633
Darrell <darrell78383@hotmail.com>
2021-11-12 12:35:37 +1100

    Added checkBuildables for armour (armour is a material)

--- src/main/java/Entities/collectableEntities/equipments/Armour.java
+++ src/main/java/Entities/collectableEntities/equipments/Armour.java
@@ -1,11 +1,27 @@
 package Entities.collectableEntities.equipments;
 
+import Entities.Entities;
 import Entities.collectableEntities.CollectableEntity;
+import Entities.movingEntities.Character;
+import dungeonmania.Dungeon;
 import dungeonmania.util.Position;
 
 public class Armour extends CollectableEntity {
 
     public Armour(String id, Position position) {
         super(id, "armour", position, false);
+    }
+
+    /**
+     * @param dungeon
+     * @param walker
+     */
+    @Override
+    public void walkedOn(Dungeon dungeon, Entities walker) {
+        if (walker instanceof Character) {
+            Character character = (Character) walker;
+            pickup(dungeon, character);
+            character.checkForBuildables(dungeon, null);
+        }
     }
 }
